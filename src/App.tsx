2207873d153e6a749cd48dc96c56f0bad27af853
Ksenia PI <ksenia.pidopryhora@gmail.com>
2022-12-07 01:17:12 +0200

    update fetchUsers

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
+import TableComponent from "./components/Table";
+import { Table } from "react-bootstrap";
+import { IUser } from "./types/types";
 
 function App() {
   const baseUrl = "http://localhost:3004/user";
 
+  const [users, setUsers] = useState<IUser[]>([]);
+  console.log(users);
+
   useEffect(() => {
-    axios
-      .get(baseUrl)
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((error) => {
-        // handle error
-        console.log(error);
-      });
+    fetchUsers();
   }, []);
+
+  async function fetchUsers() {
+    try {
+      const response = await axios.get<IUser[]>(baseUrl);
+      setUsers(response.data);
+    } catch (error) {
+      alert(error);
+    }
+  }
 
   return (
     <div className="app">
-      <h3>App</h3>
+      <Table responsive>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Work</th>
+            <th>hobby</th>
+          </tr>
+        </thead>
+        <tbody>
+          <TableComponent users={users} />
+        </tbody>
+      </Table>
     </div>
   );
 }
