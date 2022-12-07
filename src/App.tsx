03c1e9868a3bb208ff4fe31d5d35aa1b632e3e39
Ksenia PI <ksenia.pidopryhora@gmail.com>
2022-12-07 04:33:01 +0200

    add infinite loader

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,70 @@ import TableComponent from "./components/Table";
 import { Table } from "react-bootstrap";
 import { IUser } from "./types/types";
 
+// interface TotalCount {
+//   "set-cookie"?: string[] | undefined | number;
+// }
+
 function App() {
-  const baseUrl = "http://localhost:3004/user";
+  //const baseUrl = "http://localhost:3004/user?_limit=30";
 
   const [users, setUsers] = useState<IUser[]>([]);
   console.log(users);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [fetching, setFetching] = useState(true);
+  // const [totalCount, setTotalCount] = useState<TotalCount>(0);
 
   useEffect(() => {
-    fetchUsers();
-  }, []);
+    // if (fetching) {
+      console.log("fetching");
+      axios
+        .get(
+          `https://jsonplaceholder.typicode.com/comments?_limit=35&_page=${currentPage}`
+        )
+        .then((res) => {
+          console.log(res.data);
+          setUsers([...users, ...res.data]);
+          // setCurrentPage((prevState) => prevState + 1);
+          // setTotalCount(res.headers);
+        })
 
-  async function fetchUsers() {
-    try {
-      const response = await axios.get<IUser[]>(baseUrl);
-      setUsers(response.data);
-    } catch (error) {
-      alert(error);
+        // .catch(function (error) {
+        //   // handle error
+        //   console.log(error);
+        // })
+        .finally(() => setFetching(false));
+    // }
+  }, [currentPage]);
+
+  const scrollToEnd = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  window.onscroll = function () {
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      scrollToEnd();
     }
-  }
+  };
+
+  // useEffect(() => {
+  //   document.addEventListener("scroll", scrollHandler);
+  //   return function () {
+  //     document.removeEventListener("scroll", scrollHandler);
+  //   };
+  // }, []);
+
+  // const scrollHandler = (event: Event) => {
+  //   const target = event.target as HTMLTextAreaElement;
+  //   if (
+  //     target.scrollHeight - (target.scrollTop + window.innerHeight) < 100 &&
+  //     users.length < 500
+  //   ) {
+  //     setFetching(true);
+  //   }
+  // };
 
   return (
     <div className="app">
