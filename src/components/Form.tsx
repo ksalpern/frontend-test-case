a72c8e4d8b9dae0c09e1b84cc87eecfb87d302b1
Ksenia PI <ksenia.pidopryhora@gmail.com>
2022-12-07 06:08:29 +0200

    add FormModal component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
-function Form() {
-  return (
-    <div>
-      
-    </div>
-  )
+interface ModalProps {
+  show: boolean;
+  onHide: () => void;
 }
 
-export default Form
+const FormComponent: React.FC<ModalProps> = (props) => {
+  return (
+    <>
+      <Modal show={props.show} onHide={props.onHide}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="name@example.com"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Example textarea</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={props.onHide}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={props.onHide}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default FormComponent;
