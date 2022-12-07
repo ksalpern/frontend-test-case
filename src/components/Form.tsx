936f8d6ee2675a15ef89acc06c629e1178214af7
Ksenia PI <ksenia.pidopryhora@gmail.com>
2022-12-07 13:45:56 +0200

    update Form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -11,6 +11,10 @@ interface ModalProps {
   onHide: () => void;
   handleSubmit: () => void;
   validated: boolean;
+  description: string;
+  name: string;
+  email: string;
+  id: string;
 }
 
 const FormComponent: React.FC<ModalProps> = (props) => {
@@ -28,61 +32,49 @@ const FormComponent: React.FC<ModalProps> = (props) => {
           >
             <Row className="mb-3">
               <Form.Group as={Col} md="4" controlId="validationCustom01">
-                <Form.Label>First name</Form.Label>
+                <Form.Label>Id</Form.Label>
                 <Form.Control
                   required
-                  type="text"
-                  placeholder="First name"
-                  defaultValue="Mark"
+                  type="string"
+                  placeholder="Id"
+                  // defaultValue="Mark"
                 />
                 <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
               </Form.Group>
-              <Form.Group as={Col} md="4" controlId="validationCustom02">
-                <Form.Label>Last name</Form.Label>
+              <Form.Group as={Col} md="4" controlId="validationCustom01">
+                <Form.Label>Name</Form.Label>
                 <Form.Control
                   required
                   type="text"
-                  placeholder="Last name"
-                  defaultValue="Otto"
+                  placeholder="Name"
+                  // defaultValue="Mark"
                 />
                 <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-                <Form.Label>Username</Form.Label>
-                <InputGroup hasValidation>
-                  <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-                  <Form.Control
-                    type="text"
-                    placeholder="Username"
-                    aria-describedby="inputGroupPrepend"
-                    required
-                  />
-                  <Form.Control.Feedback type="invalid">
-                    Please choose a username.
-                  </Form.Control.Feedback>
-                </InputGroup>
               </Form.Group>
             </Row>
             <Row className="mb-3">
+              <Form.Group as={Col} md="10" controlId="validationCustom02">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  required
+                  type="email"
+                  placeholder="Email"
+                  // defaultValue="test@gmail.co"
+                />
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              </Form.Group>
+            </Row>
+            <Row className="mb-4">
               <Form.Group as={Col} md="6" controlId="validationCustom03">
-                <Form.Label>City</Form.Label>
-                <Form.Control type="text" placeholder="City" required />
+                <Form.Label>Description</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  placeholder="Description"
+                  required
+                />
                 <Form.Control.Feedback type="invalid">
                   Please provide a valid city.
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group as={Col} md="3" controlId="validationCustom04">
-                <Form.Label>State</Form.Label>
-                <Form.Control type="text" placeholder="State" required />
-                <Form.Control.Feedback type="invalid">
-                  Please provide a valid state.
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group as={Col} md="3" controlId="validationCustom05">
-                <Form.Label>Zip</Form.Label>
-                <Form.Control type="text" placeholder="Zip" required />
-                <Form.Control.Feedback type="invalid">
-                  Please provide a valid zip.
                 </Form.Control.Feedback>
               </Form.Group>
             </Row>
